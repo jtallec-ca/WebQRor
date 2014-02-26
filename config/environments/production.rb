7d01d169575da3f429a2a6f18a3badbc477999c3
Julien Tallec <service.github@tjtallec.ca>
2014-02-26 06:48:02 -0800

    modify production env settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ WebQ::Application.configure do
 
   #Mailer config
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.zoho.com",
